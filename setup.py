ef55f7de444bc20dc124f49bb95b8813cffc6079
zongdaoming <m15901768536@163.com>
2021-03-13 21:22:38 +0800

    "edit setup.py"

---
+++ setup.py
@@ -0,0 +1,28 @@
+# _*_ coding: utf-8 _*_
+# @author  :   naive dormin
+# @time    :   2021/03/13 21:21:09
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+REQUIRED_PACKAGES = ['numpy', 'dm-sonnet==1.35', 'tensorflow==1.14',
+                     'tensorflow-probability==0.7.0']
+
+setup(
+    name='hpu_net',
+    version='0.1',
+    description='A library for the Hierarchical Probabilistic U-Net model.',
+    url='https://github.com/deepmind/deepmind-research/hierarchical_probabilistic_unet',
+    author='DeepMind',
+    author_email='no-reply@google.com',
+    # Contained modules and scripts.
+    packages=find_packages(),
+    install_requires=REQUIRED_PACKAGES,
+    platforms=['any'],
+    license='Apache 2.0',
+)
