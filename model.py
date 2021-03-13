d8725f5157b43139ad27b25f689145dbe81c5957
zongdaoming <m15901768536@163.com>
2021-03-13 20:51:09 +0800

    "first draft to main model"

---
+++ model.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+# @author  :   naive dormin
+# @time    :   2021/03/13 20:47:29
+
+import geco_utils
+import sonnet as snt
+import tensorflow as tf
+from tensorflow_probability import distributions as tfd
+import unet_utils
+
+
